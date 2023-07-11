b16de3348a1e228d5867a3c7811bf5d3d4094970
Brian Nguyen <briannguyen636@gnail.com>
2023-07-11 14:37:33 -0700

    rename

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 	
-	gameEngine.addEntity(new Youmu(gameEngine));
+	gameEngine.addEntity(new Player(gameEngine));
 
 	gameEngine.init(ctx);
 	gameEngine.currentRoom = new RoomManager();
